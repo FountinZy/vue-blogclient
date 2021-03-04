eb8937e8193ac45d9f925b9d37d20c8257d9b5c3
FountinZy <Z951221zz@gmail.com>
2021-03-04 17:11:06 +0800

    init

---
+++ src/components/footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer>
+    <p>© zyfind.com 2021 wechat: 15707053240</p>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  align-self: end;
+  background-color: #d7d7d7;
+  color: #666;
+  font-size: 13px;
+  padding: 10px;
+  text-align: center;
+}
+
+</style>
